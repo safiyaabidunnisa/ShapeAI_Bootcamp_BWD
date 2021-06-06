89d7c94bafa68209773d49e922eb7f2f61084fe1
safiyaabidunnisa <85436504+safiyaabidunnisa@users.noreply.github.com>
2021-06-06 16:19:10 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Header() {
+  return (
+    <header>
+      <h1>
+        <center>
+          <u>ShapeAI</u>
+        </center>
+      </h1>
+    </header>
+  );
+}
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+function Note() {
+  return (
+    <div>
+      <div className="note">
+        <h1>Full-Stack Web Dev Training and Internship</h1>
+        <p>
+          The goal of this Training and Internship Program is to equip interns
+          with the unique skills they need to build database-backed APIs and web
+          applications with the proper industry level front-end as well as scale
+          up the deployment for the millions. A graduate of this program will be
+          able to design and build databases for software applications, create
+          and deploy database-backed web APIs, and secure and manage user
+          authentication and access control for an application backend. They
+          will be able to build production level frontend according to the
+          industrial standards. Students will also learn how to deploy a the
+          entire web application to the cloud using Docker and Kubernetes and
+          how to maintain and scale them.
+        </p>
+      </div>
+      <div className="note">
+        <h1>Data Analyst Training and Internship</h1>
+        <p>
+          The Goal of this Training and Internship program is to Advance your
+          programming skills and refine your ability to work with messy, complex
+          datasets. You’ll learn to manipulate and prepare data for analysis,
+          and create visualizations for data exploration. Finally, you’ll learn
+          to use your data skills to tell a story with data. In this program,
+          you’ll learn foundational data skills that apply across functions and
+          industries. You’ll learn to analyze data and build models with Excel,
+          query databases using SQL, and create informative data visualizations
+          with Python and its Libraries.
+        </p>
+      </div>
+      <div className="note">
+        <h1>
+          Web Dev with Javascript and React.js - Free 7 days Online Bootcamp
+        </h1>
+        <p>
+          Here we bring our another free 7 days live BOOTCAMP with a hands-on
+          project at the end of Front-End Web Dev with Javascript and React.js-
+          The Foundation of Frontend Web Development
+        </p>
+      </div>
+      <div className="note">
+        <h1>Python and Machine Learning Free 7 Days Online Bootcamp </h1>
+        <p>
+          Here we bring our another free live BOOTCAMP with a hands-on project
+          at the end; Python and Machine Learning- The Foundation of Data
+          Science and Artificial Intelligence
+        </p>
+      </div>
+    </div>
+  );
+}
+export default Note;
